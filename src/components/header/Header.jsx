fb7b22bbf4e90447026bdfdfd24e0b76f087eb01
Lucas Mizuta <mswmizuta@gmail.com>
2023-04-03 15:14:27 -0300

    Criado componente de icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,19 @@ import styles from './Header.module.css'
 
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
+import Icon from  '../icon/Icon'
 
 function Header () {
   return (
+
   <div className={styles.header}>
     <Title>Jogo da Velha</Title>
     <Subtitle>Criado por Lucas Mizuta</Subtitle>
+  <div className={styles.iconContent}>
+    <Icon iconName='github' size='30px' link="https://github.com/l-mizuta" />
   </div>
+  </div>
+
   )
 }
 
